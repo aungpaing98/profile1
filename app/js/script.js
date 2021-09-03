469952025b7b6ac6d7deaf8d499ef2e14765eba7
aungpaing98 <aungpaingcha@gmail.com>
2021-09-03 10:33:51 +0630

    Add features

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,37 @@
-console.log("Hello")
+// const email = document.querySelector('.email');
+// const github = document.querySelector('.github');
+// const linkedin = document.querySelector('.linkedin');
+// const youtube = document.querySelector('.youtube');
+
+// email.addEventListener('click', ()=>{
+//     const email_link = document.createElement('input')
+//     email_link.textContent = 'aungpaingcha1@gmail.com';
+//     email_link.select();
+//     document.execCommand('copy', true, email_link.value);
+// })
+
+// github.addEventListener('click', ()=>{
+//     // const github_link = document.createElement('input')
+//     // github_link.textContent = 'https://github.com/aungpaing98';
+//     // github_link.select();
+//     // document.execCommand('copy', true, github_link.value);
+//     // console.log("Github Click")
+// })
+
+// linkedin.addEventListener('click', ()=>{
+//     // const linkedin_link = document.createElement('input')
+//     // linkedin_link.textContent = 'https://www.linkedin.com/in/aungpaing98/';
+//     // linkedin_link.select();
+//     // document.execCommand('copy', true, linkedin_link.value);
+//     // console.log("LinkedIN Click")
+// })
+
+
+// youtube.addEventListener('click', ()=>{
+//     // const youtube_link = document.createElement('input')
+//     // youtube_link.textContent = 'https://www.youtube.com/channel/UC6Vhf_yoIWXiLLJqTrU0FWA';
+
+//     // youtube_link.select();
+//     // document.execCommand('copy', true, youtube_link.value);
+//     // console.log("YOuutube Click")
+// })
